ba1132d43c08fa591daf8323b5947896948c48f1
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2016-04-05 09:58:31 -0700

    Add increment/decrement buttons
    Adds buttons to layout and connects them to the added increment/decrement
    methods.

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,6 +24,16 @@ public class MainActivity extends ActionBarActivity {
         displayPrice(quantity * 5);
     }
 
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
     /**
      * This method displays the given quantity value on the screen.
      */
