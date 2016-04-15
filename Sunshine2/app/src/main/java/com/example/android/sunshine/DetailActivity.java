564f6d3502714f3a5bf4718c1f34863126e7e4e4
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-04-15 09:08:55 -0700

    Add ShareActionProvider to DetailActivity
    Adds Android-standard menu items for sharing information via the
    ShareActionProvider. These will share the data displayed on the
    DetailActivity page as a string appended with "#SunshineApp". Also
    removes the unused Settings menu item from DetailActivity's menu.

--- Sunshine2/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Sunshine2/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,34 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        // Location MenuItem with ShareActionProvider
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        if (mShareActionProvider != null)
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+
         return true;
+    }
+
+    private Intent createShareForecastIntent() {
+        // Get the intent from the fragment's activity
+        Intent activityIntent = getIntent(), shareIntent = null;
+
+        // Ensure activity's intent exists and has extra text
+        if (activityIntent != null && activityIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecast = activityIntent.getStringExtra(Intent.EXTRA_TEXT);
+
+            // Create share intent
+            shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + "#SunshineApp");
+            shareIntent.setType("text/plain");
+        }
+
+        return shareIntent;
     }
 
     @Override
